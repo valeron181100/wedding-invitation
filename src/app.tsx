ccbcddb5e31414606b917c5827a329c7361280d8
Valeriy Bondar <bondar2501@outlook.com>
2026-02-10 02:23:11 +0300

    step 8 added scroll tip

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@emotion/react";
 import { SnackbarProvider } from "notistack";
+import { useEffect, useRef, useState } from "react";
 import { Body } from "./layout";
 import { MainSection } from "./sections/main-section/main-section";
 import { theme } from "./theme";
@@ -8,12 +9,40 @@ import { WeddingCeremonyInvitationSection } from "./sections/wedding-ceremony-in
 import { CelebratingInvitationSection } from "./sections/celebrating-invitation-section/celebrating-invitation-section";
 import { FormSection } from "./sections/form-section/form-section";
 import { ByeSection } from "./sections/bye-section/bye-section";
+import ScrollTip from "./components/view/scroll-tip/scroll-tip";
 
 const App = () => {
+	const bodyRef = useRef<HTMLDivElement>(null);
+
+	const [showScrollTip, setShowScrollTip] = useState(() => {
+		return localStorage.getItem("hasSeenScrollTip") !== "true";
+	});
+
+	useEffect(() => {
+		const scrollElement = bodyRef.current;
+
+		if (!scrollElement || !showScrollTip) return;
+
+		const handleScroll = () => {
+			if (scrollElement.scrollTop > window.innerHeight - 100) {
+				localStorage.setItem("hasSeenScrollTip", "true");
+				setShowScrollTip(false);
+			}
+		};
+
+		scrollElement.addEventListener("scroll", handleScroll);
+
+		// eslint-disable-next-line consistent-return
+		return () => {
+			scrollElement.removeEventListener("scroll", handleScroll);
+		};
+	}, [showScrollTip]);
+
 	return (
 		<ThemeProvider theme={theme}>
 			<SnackbarProvider maxSnack={1}>
-				<Body>
+				<Body ref={bodyRef}>
+					<ScrollTip visible={showScrollTip} />
 					<MainSection />
 					<InviteMessageSection />
 					<WeddingCeremonyInvitationSection />
---
+++ src/components/view/scroll-tip/scroll-tip.tsx
@@ -0,0 +1,51 @@
+import "./styles.css";
+import { AnimatePresence, motion } from "framer-motion";
+
+interface ScrollTipProps {
+	visible: boolean;
+}
+
+const ScrollTip = ({ visible = false }: ScrollTipProps) => {
+	const color = "#ffffff";
+
+	return (
+		<AnimatePresence>
+			{visible && (
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					className="scroll-tip-container"
+				>
+					<p
+						className="scroll-text"
+						style={{
+							color,
+						}}
+					>
+						Листайте вниз
+					</p>
+					<div className="scroll-arrow">
+						<svg
+							width="30"
+							height="30"
+							viewBox="0 0 24 24"
+							stroke={color}
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							style={{
+								transform: "scale(2.5)",
+								filter: "drop-shadow( 0px 0px 2px rgba(0, 0, 0, .7))",
+							}}
+						>
+							<path d="M7 13l5 5 5-5M7 6l5 5 5-5" fill={"transparent"} />
+						</svg>
+					</div>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	);
+};
+
+export default ScrollTip;
